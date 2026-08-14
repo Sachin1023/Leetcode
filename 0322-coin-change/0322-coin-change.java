6748641f3c20b66c191e1851dd63b23cb14bdd98
Sachin1023 <85932991+Sachin1023@users.noreply.github.com>
2026-08-14 15:21:54 +0530

    Time: 17 ms (49.29%), Space: 46.9 MB (30.68%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,21 +1,43 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[] dp = new int[amount + 1];
-        int max = amount+1;
-
-        // Initialize
-        for (int i = 1; i <= amount; i++) {
-            dp[i] = max;
-        }
-        dp[0] = 0;  // base case
-
-        // Fill DP
-        for (int coin : coins) {
-            for (int j = coin; j <= amount; j++) {
-                dp[j] = Math.min(dp[j], 1 + dp[j - coin]);
-            }
-        }
-
-        return dp[amount] == max ? -1 : dp[amount];
+       int n=coins.length;
+       int res= unboundedKnapsack(coins,amount,n);
+        return  res== Integer.MAX_VALUE-1 ? -1 : res;
     }
+
+    public int unboundedKnapsack(int []coins, int amount ,int n)
+    {
+       int t[][]= new int[n+1][amount+1];
+       //initilizaition;
+       int inf = Integer.MAX_VALUE-1;
+
+       for(int j=0;j<=amount ;j++)
+       {
+            t[0][j]=inf;
+       }
+       //for 2nd row
+       for(int j=1;j<=amount;j++)
+       {
+            if( j%coins[0]==0)
+            {
+                t[1][j]=j/coins[0];
+            }
+            else
+                t[1][j]=inf;
+       }
+
+       for(int i=2;i<=n;i++)
+       {
+        for(int j=1;j<=amount;j++)
+        {
+            if(coins[i-1]<=j)
+            {
+                t[i][j]=Math.min(t[i-1][j] ,1+t[i][j-coins[i-1]]);
+            }else
+                t[i][j]=t[i-1][j];
+        }
+       }
+       return t[n][amount];
+    }
+    
 }
