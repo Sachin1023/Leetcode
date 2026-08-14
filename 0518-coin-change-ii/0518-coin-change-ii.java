624e6b720fc11f306613e85fd48cd87f75b21a9f
Sachin1023 <85932991+Sachin1023@users.noreply.github.com>
2026-08-14 15:22:01 +0530

    Time: 12 ms (48.51%), Space: 56.9 MB (37.66%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,31 +1,27 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int n=coins.length;
-        return unboundedKnapsack(coins,amount,n);
+        return unboundedknapsack(coins,coins.length,amount);
     }
 
-    public int unboundedKnapsack(int []coins, int amount ,int n)
+    public int unboundedknapsack(int wt[], int n, int w)
     {
-        int [][]t = new int[n+1][amount+1];
-        for(int a[]: t)//initialization
+        int t[][]= new int[n+1][w+1];
+        //initialization
+        for(int i=0;i<=n;i++)
         {
-            a[0]=1;
+            t[i][0]=1;
         }
-        
+
         for(int i=1;i<=n;i++)
         {
-            for(int j=0;j<=amount;j++)
-            {
-                if(coins[i-1]<=j)
-                {
-                    t[i][j]=t[i-1][j]+t[i][j-coins[i-1]];
-                }
+            for(int j=1;j<=w;j++)
+            {   
+                if(wt[i-1]<=j)
+                    t[i][j]=t[i-1][j]+t[i][j-wt[i-1]];
                 else
-                {
-                    t[i][j]=t[i-1][j]; //processed, not take
-                }
+                    t[i][j]=t[i-1][j];
             }
         }
-        return t[n][amount];
+        return t[n][w];
     }
 }
