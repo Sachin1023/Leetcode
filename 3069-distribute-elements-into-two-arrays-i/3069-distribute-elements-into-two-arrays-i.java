c3a9dd08f49a527beb59f079fa7cdf70fb89642c
Sachin1023 <85932991+Sachin1023@users.noreply.github.com>
2026-08-20 16:07:15 +0530

    Time: 1 ms (98.08%), Space: 46.4 MB (95.43%) - LeetHub

--- 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
@@ -1,28 +1,59 @@
 class Solution {
     public int[] resultArray(int[] nums) {
         int n=nums.length;
-        int a[] = new int[n];
-        int b[]= new int[n];
-        int i=0, j=0;
+        //try using 1 array.
+        int a [] = new int [n];
+        int i=0, j=n-1;
         a[i++]=nums[0];
-        b[j++]=nums[1];
-
-        //iteration of nums 
-        for(int k=2; k<n;k++)
+        a[j--]=nums[1]; // for second array . adding data in reverse for it.
+        for(int k=2 ;k<n;k++)
         {
-            if(a[i-1]>b[j-1])
-            {   
+            if( a[i-1] > a[j+1])
+            {
                 a[i++]=nums[k];
             }
             else{
-                b[j++]=nums[k];
+                a[j--]=nums[k];
             }
-        }
-        j=0; // not using a new array for result using the first array and adding array 2 data into it
-        while( b[j]!=0)
+        }   
+
+        //now we need to reverse the data of a[0---i,j----n] for the j portion .
+        // after last increment i will be in correct place to iterate.
+        // System.out.println(i); 
+        j=n-1;
+        while(i<j)
         {
-            a[i++]=b[j++];
+            int temp = a[j];
+            a[j]=a[i];
+            a[i]=temp;
+            i++;
+            j--;
         }
+
+        //using two array.
+        // int a[] = new int[n];
+        // int b[]= new int[n];
+        // int i=0, j=0;
+        // a[i++]=nums[0];
+        // b[j++]=nums[1];
+
+        // //iteration of nums 
+        // for(int k=2; k<n;k++)
+        // {
+        //     if(a[i-1]>b[j-1])
+        //     {   
+        //         a[i++]=nums[k];
+        //     }
+        //     else{
+        //         b[j++]=nums[k];
+        //     }
+        // }
+        // j=0; // not using a new array for result using the first array and adding array 2 data into it
+        // while( b[j]!=0)
+        // {
+        //     a[i++]=b[j++];
+        // }
+
         return a;
 
     }
