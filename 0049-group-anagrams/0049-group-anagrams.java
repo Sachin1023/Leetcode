1e730e04445bdfd8b2cb718fe063263d879050ed
Sachin1023 <85932991+Sachin1023@users.noreply.github.com>
2026-09-04 23:23:02 +0530

    Time: 18 ms (20.73%), Space: 49.2 MB (95.99%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,26 +1,39 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        // List<List<String>> ans = new ArrayList<>();
+        List<List<String>> ans = new ArrayList<>();
+       
         Map<String,List<String>> map = new HashMap<>();
 
         for(String word : strs) 
         {
-            // int count[]  = new int [26]; // 26 lower case alphabets;
-            // for(char c : word.toCharArray())
-            // {
-            //     count[ c-'a']++;
-            // }
-            char[] c=word.toCharArray();
-            Arrays.sort(c);
-            String key = new String(c);
-            map.computeIfAbsent(key, k -> new ArrayList<>()).add(word);
-            // map.computeIfAbsent(1, k -> new ArrayList<>()).add("Hello");
-
-            // map.putIfAbsent(count, new ArrayList<>());
-            // map.get(count).add(word);
+            int count[]  = new int [26]; // 26 lower case alphabets;
+            for(char c : word.toCharArray())
+            {
+                int ind = c-'a';
+                count[ind]++;
+            }
+             String key = Arrays.toString(count);
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(word);
         }
-        // ans.addAll(map.values());
-        // return ans;
-        return new ArrayList<>(map.values());
+        ans.addAll(map.values());
+        return ans;
     }
+    // for( String s : strs)
+    //     {   
+    //         int [] count = new int [26];
+
+    //         for(char c : s.toCharArray())
+    //         {
+    //             count[c - 'a']++;
+    //         }
+
+    //         String key = Arrays.toString(count);
+
+    //         map.putIfAbsent(key, new ArrayList<>());
+    //         map.get(key).add(s);
+        
+    //     }
+    //     return new ArrayList<>(map.values());
+    
 }
